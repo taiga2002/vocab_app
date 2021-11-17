ca14d965606042066b90f5a91e9cee58ffb6f781
Taiga Kitao <taiga20022002@gmail.com>
2021-11-16 21:45:20 -0800

    Initial Commit

---
+++ Flash/FlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashApp.swift
+//  Flash
+//
+//  Created by 北尾　大河 on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct FlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
